b28ff7e16575b9a81a8e50cc67c16b3fa08d57ec
Prince Nwaonicha <lolgfman@gmail.com>
2022-07-31 00:43:38 -0500

    Finished Dynamic Programming

--- Algorithms/Dynamic Programming/DynamicProgramming.js
+++ Algorithms/Dynamic Programming/DynamicProgramming.js
@@ -44,3 +44,49 @@ function memoizedAddTo80() {
 const memoized = memoizedAddTo80()
 console.log(memoized(70))
 console.log(memoized(70))
+
+///0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233...
+
+let calculations1 = 0;
+let calculations2 = 0;
+function fibonacci(n) {// O(2^n) Time complexity
+  calculations1++;
+  if (n < 2) {
+    return n
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+//top down approach
+function fibonacciMaster() {// O(n) Time complexity
+  let cache = {};// O(n)
+  return function fib(n) {
+    calculations2++
+    if (n in cache) {
+      return cache[n]
+    } else {
+      if (n < 2) {
+        return n
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n]
+      }
+    }
+  }
+}
+
+//bottom up approach
+function fibonacciMaster2(n) {
+  let answer = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    answer.push(answer[i - 2] + answer[i - 1]);
+  }
+  return answer.pop();
+}
+
+fasterFibb = fibonacciMaster();
+
+console.log(fasterFibb(1000));
+console.log(calculations2)
+console.log(fibonacci(30))
+console.log(calculations1)
